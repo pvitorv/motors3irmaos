c2094b39603c49dec6f0238974a6733b22f55aa2
Paulo Vitor <pvitorv@gmail.com>
2024-06-06 19:46:41 -0300

    0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,26 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>TESTE PAGINA</h1>
+      <div 
+      style={{ width:"100%", height:600, backgroundColor: "#121212", marginBottom: 
+      24}}
+      >
+        <h1>TESTE PAGINA</h1>
+      </div>
+
+      <div 
+      style={{ width:"100%", height:600, backgroundColor: "blue", marginBottom: 
+      24}}
+      >
+        <h1>TESTE PAGINA</h1>
+      </div>
+
+      <div 
+      style={{ width:"100%", height:600, backgroundColor: "green", marginBottom: 
+      24}}
+      >
+        <h1>TESTE PAGINA</h1>
+      </div>
     </main>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,57 @@
+"use client"
+
+import {useState, useEffect } from 'react'
 import styles from './style.module.scss'
+import Link from 'next/link'
 
 
 export function Header(){
+
+    const[top, setTop] = useState(true);
+
+    const scrollHandler = () => {
+        window.scrollY > 10 ? setTop(false) : setTop(true);
+    }
+
+    useEffect(() => {
+
+        window.addEventListener("scroll", scrollHandler)
+
+        return () => window.removeEventListener("scroll", scrollHandler)
+
+    }, [top])
+
     return(
-        <header>
-            <h2>TESTE</h2>
+        <header className={`${styles.header} ${!top ? styles.fixed : styles.background }`}>
+
+            <div className={styles.container}>
+
+                <div className={styles.content}>
+
+                    <div className={styles.contentLogo}>
+                        <a href='/'>
+                            Motors 3 Irmãos
+                        </a>
+                    </div>
+
+                    <nav className={styles.nav}>
+                        <a href='/'>
+                            HOME
+                        </a>
+
+                        <a href='/#servicos'>
+                            SERVIÇOS
+                        </a>
+
+                        <a href='/#contatos'>
+                            CONTATOS
+                        </a>
+                    </nav>
+
+                </div>
+
+            </div>
+
         </header>
     )
 }
