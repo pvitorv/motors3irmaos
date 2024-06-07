a872b4bb1e5ca4f9c2b9f42c10f4696a5f2668cf
Paulo Vitor <pvitorv@gmail.com>
2024-06-07 10:55:14 -0300

    0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <div 
-      style={{ width:"100%", height:600, backgroundColor: "#121212", marginBottom: 
-      24}}
-      >
-        <h1>TESTE PAGINA</h1>
-      </div>
 
-      <div 
-      style={{ width:"100%", height:600, backgroundColor: "blue", marginBottom: 
-      24}}
-      >
-        <h1>TESTE PAGINA</h1>
-      </div>
-
-      <div 
-      style={{ width:"100%", height:600, backgroundColor: "green", marginBottom: 
-      24}}
-      >
-        <h1>TESTE PAGINA</h1>
-      </div>
     </main>
   );
 }
