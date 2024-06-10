0d0259a28cfce9de867b8123e9897e292c601418
Paulo Vitor <pvitorv@gmail.com>
2024-06-10 01:25:29 -0300

    Hiro ficou completo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import { Submenu } from "@/components/home/submenu";
 import {getDataHome} from '@/utils/actions/get-data'
 import {HomeProps} from "@/utils/actions/home.type"
+import {Hero} from '@/components/hero'
+import { Phone } from "lucide-react";
 
 export default async function Home() {
   const {object}: HomeProps = await getDataHome();
@@ -10,6 +12,14 @@ export default async function Home() {
   return (
     <main>
       <Submenu/>
+
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#fff"/>}
+      />
     </main>
   )
 }
